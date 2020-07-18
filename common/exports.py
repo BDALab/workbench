3f4445dc64d890178434284d2b070a90d921a650
Zoltán Galáž <xgalaz00@gmail.com>
2020-07-18 13:10:08 +0200

    Added function for storing multiple excel sheets

--- common/exports.py
+++ common/exports.py
@@ -5,6 +5,14 @@ from common.file_system import ensure_directory
 def save_to_excel(df, output_path, sheet_name="Sheet1", index=False):
     """Save the <df> as excel sheet"""
     ensure_directory(output_path)
-    with pd.ExcelWriter(output_path) as writer:
+    with pd.ExcelWriter(output_path, engine="xlsxwriter") as writer:
         df.to_excel(writer, sheet_name=sheet_name, index=index)
+        writer.save()
+
+
+def save_to_excel_multi(dfs, output_path, sheet_names=None, index=False):
+    ensure_directory(output_path)
+    with pd.ExcelWriter(output_path, engine="xlsxwriter") as writer:
+        for n, df in enumerate(dfs):
+            df.to_excel(writer, sheet_name=sheet_names[n] if sheet_names else f"Sheet{n}", index=index)
         writer.save()
